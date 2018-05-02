37d7900bbb880e89223ccf67cefaa7628624dfbb
Student X <simen1127@gmail.com>
2018-05-02 10:22:48 +0800

    add defaut data to privacy and category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+category_list = [
+  { name: "商業類"},
+  { name: "技術類"},
+  { name: "心理類"}
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+end
+
+puts "Category created!"
+
+
+Privacy.destroy_all
+
+privacy_list = [
+  { name: "ALL"},
+  { name: "Friend"},
+  { name: "Myself"}
+]
+
+privacy_list.each do |privacy|
+  Privacy.create( name: privacy[:name])
+end
+
+puts "Privacy created!"
