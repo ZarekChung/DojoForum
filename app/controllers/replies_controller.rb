157d8a3609ff6e9a9101c666f16517602bc43874
Student X <simen1127@gmail.com>
2018-05-07 17:43:37 +0800

    edit reply at same page

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -16,6 +16,24 @@ class RepliesController < ApplicationController
     #end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @reply = Reply.find(params[:id])
+    render :layout => false
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @reply = Reply.find(params[:id])
+    if @reply.update_attributes(reply_params)
+      flash[:notice] = "Reply was scuccessfully updated"
+      redirect_to post_path(@post)
+    else
+      flash.now[:alert] ="Reply was failed to update"
+      render :edit
+    end
+  end
+
   def reply_params
    params.require(:reply).permit(:content)
  end
