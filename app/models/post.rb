b895b048dc9c3e736a8883a7da7aca309ce43d90
Zarek <simen1127@gmail.com>
2018-05-14 00:55:52 +0800

    add validates_presence_of post title

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   mount_uploader :photo, PhotoImageUploader
+  validates_presence_of :title
   has_many :category_of_posts, dependent: :destroy
   belongs_to :privacy
   belongs_to :user
